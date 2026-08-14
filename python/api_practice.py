4a290510c190a1baf5261a8ac448a590e57a7736
Honey-Gupta-06 <honeygupta891@gmail.com>
2026-08-14 21:18:02 +0530

    build random quote API program

--- python/api_practice.py
+++ python/api_practice.py
@@ -1,14 +1,44 @@
 import requests
 
-url = "https://dummyjson.com/quotes/random"
 
-response = requests.get(url)
+def get_quote():
+    url = "https://dummyjson.com/quotes/random"
 
-if response.status_code == 200:
-    data = response.json()
+    try:
+        response = requests.get(url, timeout=10)
 
-    print("Quote:", data["quote"])
-    print("Author:", data["author"])
+        if response.status_code == 200:
+            data = response.json()
 
-else:
-    print("Failed to get quote.")
+            print("\n==============================")
+            print("        RANDOM QUOTE")
+            print("==============================")
+            print("Quote:", data["quote"])
+            print("Author:", data["author"])
+            print("==============================")
+
+        else:
+            print("Unable to get quote.")
+            print("Status code:", response.status_code)
+
+    except requests.exceptions.RequestException:
+        print("Network error. Please check your internet connection.")
+
+
+while True:
+
+    print("\n===== QUOTE APP =====")
+    print("1. Get Random Quote")
+    print("2. Exit")
+
+    choice = input("Enter your choice: ")
+
+    if choice == "1":
+        get_quote()
+
+    elif choice == "2":
+        print("Goodbye!")
+        break
+
+    else:
+        print("Invalid choice. Please try again.")
