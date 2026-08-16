a0664a66a80078ad2565693895f5c83cc6d70927
Honey-Gupta-06 <honeygupta891@gmail.com>
2026-08-16 17:36:33 +0530

    data cleaning and processing

--- python/eda.py
+++ python/eda.py
@@ -1,5 +1,4 @@
 import pandas as pd
-
 df = pd.read_csv("python/student_data.csv")
 
 print("Student Data:")
@@ -15,3 +14,26 @@ print(df.dtypes)
 
 print("\nBasic Statistics:")
 print(df.describe())
+
+print("\nMissing Values:")
+print(df.isnull().sum())
+
+print("\nStudents scoring above 80:")
+print(df[df["Marks"] > 80])
+
+print("\nStudents from Ranchi:")
+print(df[df["City"] == "Ranchi"])
+
+average_marks = df["Marks"].mean()
+print("\nAverage Marks:", average_marks)
+
+print("Highest Marks:", df["Marks"].max())
+
+print("Lowest Marks:", df["Marks"].min())
+
+print("\nAverage Marks by Course:")
+print(df.groupby("Course")["Marks"].mean())
+
+print("\nAverage Marks by City:")
+print(df.groupby("City")["Marks"].mean())
+
